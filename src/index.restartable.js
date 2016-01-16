050ab78fe9fb0aa3f2301595d6b73ccdf21691a5
Mike Montoya <dr.mike.montoya@gmail.com>
2016-01-16 00:48:55 -0600

    Another attempt with cycle-restart.

--- src/index.restartable.js
+++ src/index.restartable.js
@@ -3,6 +3,7 @@ import {makeDOMDriver} from 'cycle-snabbdom'
 import {makeHTTPDriver} from '@cycle/http'
 import {makeHistoryDriver} from '@cycle/history'
 let app = require('./app').default
+import 'normalize.css'
 import '../css/app.scss' //webpack will process this
 
 //packages needed for hot reloading w/ preserved state
@@ -11,16 +12,18 @@ import {restart, restartable} from 'cycle-restart'
 
 const ROOT_SELECTOR = '.app-container'
 
+const snabbdomModules = [
+  require(`snabbdom/modules/class`),
+  require(`snabbdom/modules/hero`),
+  require(`snabbdom/modules/style`),
+  require(`snabbdom/modules/props`),
+  require(`snabbdom/modules/attributes`),
+]
+
 const drivers = {
-  DOM: restartable(makeDOMDriver(ROOT_SELECTOR, [
-    require(`snabbdom/modules/class`),
-    require(`snabbdom/modules/hero`),
-    require(`snabbdom/modules/style`),
-    require(`snabbdom/modules/props`),
-    require(`snabbdom/modules/attributes`),
-  ]), {pauseSinksWhileReplaying: false}),
+  DOM: restartable(makeDOMDriver(ROOT_SELECTOR, snabbdomModules), {pauseSinksWhileReplaying: false}),
   HTTP: restartable(makeHTTPDriver()),
-  History: makeHistoryDriver({hash: false, queries: true}),
+  History: restartable(makeHistoryDriver({hash: false, queries: true}), {pauseSinksWhileReplaying: true}),
 }
 
 const {sinks, sources} = Cycle.run(app, drivers)
@@ -31,29 +34,4 @@ if (module.hot) {
     app = require('./app').default
     restart(app, drivers, {sinks, sources}, isolate)
   })
-  // module.hot.accept()
-  // module.hot.dispose(() => {
-  //   /*
-  //     SNABBDOM-SPECIFIC WORKAROUND
-  //     ----------------------------
-  //     If your Cycle app vTree's root element has a different selector (class/id) than
-  //     the original selector passed into makeDOMDriver(), you will need to uncomment
-  //     the code below to recreate the original container.  This scenario is likely
-  //     to come up if you are migrating your app from using @cycle/dom to cycle-snabbdom.
-  //     The Virtual-DOM library leaves the original container alone, while Snabbdom replaces it.
-
-  //     Alternatively, make your root element the same as the original container in your html.
-  //     This project uses this method.
-  //     index.html -> body -> <div class="app-container"></div>
-  //     Cycle app vTree root element = div('.app-container', {...}, [...])
-
-  //     //must manually remove our old DOM
-  //     document.body.removeChild(document.getElementsByClassName(PRIMARY_CONTAINER_CLASS)[0])
-  //     //snabbdom patches over the app container on initialization,
-  //     //so we have to recreate it when hot loading
-  //     let elemDiv = document.createElement('div')
-  //     elemDiv.id = 'app-container'
-  //     document.body.appendChild(elemDiv)
-  //   */
-  // })
 }
